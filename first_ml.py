0c0e77872fd2c0f21f794250ad81d6a80457668d
babak <babakrahimzadehtoosi1111@gmail.com>
2026-09-10 14:50:08 -0700

    feat(db): create database table and insert scraped data

--- first_ml.py
+++ first_ml.py
@@ -5,6 +5,7 @@ This module retrieves the country data from the website and prepares
 the extracted information for storage in a database and subsequent
 machine learning analysis.
 """
+import sqlite3
 
 import requests
 import bs4
@@ -31,5 +32,28 @@ for ins in country_data:
     ).split(",")
 
     countries.append(text)
+
+connection = sqlite3.connect("country.db")
+cursor = connection.cursor()
+
+cursor.execute("DROP TABLE IF EXISTS country_info;")
+
+cursor.execute("CREATE TABLE IF NOT EXISTS country_info("
+               "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+               "country TEXT UNIQUE NOT NULL,"
+               "capital TEXT NOT NULL,"
+               "population INTEGER NOT NULL,"
+               "area INTEGER NOT NULL)")
+
+for nations in countries:
+    cursor.execute("INSERT INTO country_info(country, capital, population, area)"
+                   " VALUES(?,?,?,?)", nations)
+
+connection.commit()
+
+
+
+
+
 
 
